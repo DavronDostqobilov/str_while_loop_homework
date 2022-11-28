34860c984804cbe30557a332628c982898e73447
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 02:40:58 +0500

    While02

--- While02.py
+++ While02.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while i<len(s):
+        if s[i].isalpha():
+            n+=1
+        i+=1
+    return n
+print(main('8787asdc8'))
