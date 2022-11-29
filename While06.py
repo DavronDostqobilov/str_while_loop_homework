41509b5c901d366be447a1541cd7fad1cc3dba7f
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 12:29:57 +0500

    While06

--- While06.py
+++ While06.py
@@ -10,8 +10,8 @@ def main(s):
     i=0
     n=0
     while i<len(s):
-        if  s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U':
+        if  s[i].lower()=='a' or s[i].lower()=='e' or s[i].lower()=='i' or s[i].lower()=='o' or s[i].lower()=='u':
             n+=1
         i+=1
     return len(s)-n
-print(main('COODES'))
+print(main('coodes'))
