f68c71765765beb92fafce2d599015d905b2cd33
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 11:20:19 +0500

    While03

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=len(s)
+    while i<len(s):
+        if s[i].isalnum():
+            n=n-1
+        i+=1
+    return n
+print(main('#davro%^n@'))
