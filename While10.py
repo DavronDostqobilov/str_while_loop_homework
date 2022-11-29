2ad27a13982fdbace62304e894d9ec15852e3373
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 11:43:26 +0500

    While10

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    sum=0
+    while i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==1:
+                sum=sum+int(s[i])
+        i+=1
+    return sum
+print(main('wd1342'))
