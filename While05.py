926bfbc364551123f5c9aa6b66b3ab505d35c945
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 11:22:44 +0500

    While05

--- While05.py
+++ While05.py
@@ -5,5 +5,12 @@ def main(s):
         s: str
     Returns:
         int: return answer
-    """
-    return
+    """ 
+    i=0
+    n=0
+    while i<len(s):
+        if s[i].islower():
+            n=n+1
+        i+=1
+    return n
+print(main('DavronD'))
