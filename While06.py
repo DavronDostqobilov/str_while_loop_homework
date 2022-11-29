97ef01c92ad033f754f122a7b0f4c6c1e7ea9e64
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 11:54:21 +0500

    While06

--- While06.py
+++ While06.py
@@ -10,8 +10,8 @@ def main(s):
     i=0
     n=0
     while i<len(s):
-        if  s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u':
+        if  s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='y':
             n+=1
         i+=1
     return len(s)-n
-print(main('Davronchik'))
+print(main('tyuydxocx'))
